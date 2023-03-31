6b5c414f0fa3b7f2634b4dad8883217a15d5d985
MawLr <mano_med_rod@hotmail.com>
2023-03-30 21:51:12 -0600

    KIA-07-reorganiza-carpetas

--- app.js
+++ app.js
@@ -2,22 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const rutasHome = require('./routes/home_routes.js')
+app.use('/', rutasHome);
 
 //app.use("/public",express.static(__dirname + '/public')); 
 //app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/public'));
-
-app.use('/', rutasHome);
+app.use("/assets",express.static(__dirname + '/assets')); 
 
 // Puerto al que escucha 3000
 app.listen(3000, ()=>{
     console.log("Server running on port", 3000)
-    console.log(__dirname + '/public')
+    console.log(__dirname + '/assets')
 });
 
