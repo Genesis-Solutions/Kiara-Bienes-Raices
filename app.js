bda116b0308640001734c9d2a755971c34ed2393
ATG-5222 <A01275222@tec.mx>
2023-03-31 16:01:56 -0600

    KIA-M-configurar app.js y añadir pagina 404

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const app = express();
+
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
 
 // Le decimos a node que nuestros assets se encuentran en assets
 app.use(express.static(path.join(__dirname, '/assets')))
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const rutasHome = require('./routes/home_routes.js')
 
@@ -15,9 +22,13 @@ app.use('/', rutasHome);
 //app.use("/public",express.static(__dirname + '/public')); 
 //app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.render('error404');
+});
+
 // Puerto al que escucha 3000
-app.listen(3000, ()=>{
-    console.log("Server running on port", 3000)
+app.listen(port, ()=>{
+    console.log("Server running on port", port)
     console.log(__dirname + '/assets')
 });
 
