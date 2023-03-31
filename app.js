f64c4387f29f4349847292085251a320539db4b5
MawLr <mano_med_rod@hotmail.com>
2023-03-30 21:17:12 -0600

    KIA-05-cambia-rutas-public

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ app.set('views', 'views');
 
 const rutasHome = require('./routes/home_routes.js')
 
-app.use("/public", express.static(path.join(__dirname, '/public'))); 
+app.use("/public",express.static(__dirname + '/public')); 
+//app.use(express.static(path.join(__dirname, 'public')));
+//app.use("/public", express.static(path.join(__dirname, '/public'))); 
 
 app.use('/', rutasHome);
 
 // Puerto al que escucha 3000
 app.listen(3000, ()=>{
     console.log("Server running on port", 3000)
-    console.log(__dirname)
+    console.log(__dirname + '/public')
 });
 
