fe8f89dde9c4358224ef178bfc32b7dd6951d63b
ATG-5222 <A01275222@tec.mx>
2023-04-11 23:19:04 -0600

    KIA-M-instalacion de dependencias para encriptacion

---
+++ models/testFile.js
@@ -0,0 +1,12 @@
+const db = require('../util/database');
+
+module.exports = class File {
+
+    static insertRegister(text,route,name) {
+        return db.execute(
+            'INSERT INTO test_file(fileDescription,fileRoute,fileName) VALUES (?,?,?)',
+            [text,route,name]
+        );
+    }
+
+}
