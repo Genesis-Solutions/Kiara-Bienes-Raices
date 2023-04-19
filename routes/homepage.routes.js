8b5402565b7cd9ecafe2e43596e9680d2d497ee3
MawLr <mano_med_rod@hotmail.com>
2023-04-19 13:27:15 -0600

    KIA-R-Rutas de Login

--- routes/homepage.routes.js
+++ routes/homepage.routes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/homepage.controller');
+const userController = require('../controllers/user.controller');
 
 // Rutas
 router.get('/', homeController.root); 
-
+router.get('/login', userController.get_login);
+router.post('/login', userController.login);
 
 
 module.exports = router;
