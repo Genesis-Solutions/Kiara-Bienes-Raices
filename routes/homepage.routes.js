d5591361b86e7aa024afeb3e62bbf6e2cc21c485
Jaime_prog <A00571842@tec.mx>
2023-04-20 09:05:33 -0600

    KIA-C-CONTROLADOR INICIAL

--- routes/homepage.routes.js
+++ routes/homepage.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/homepage.controller');
+const userController = require('../controllers/user.controller');
 
 // Rutas
 router.get('/', homeController.root); 
-
-
+router.get('/login', userController.getLogin);
+router.post('/login', userController.login);
 
 module.exports = router;
