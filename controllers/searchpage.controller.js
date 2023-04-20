ca80d5988568dec3f892ff26c308c5977a00a831
CamalichM <99758150+CamalichM@users.noreply.github.com>
2023-04-20 17:46:34 -0600

    ---

--- controllers/searchpage.controller.js
+++ controllers/searchpage.controller.js
@@ -19,11 +19,14 @@ exports.getSearchPage = async( req,res,next ) => {
     }
     //Determinar los limites
     const limiteInferior = (pagina-1)*resultadosPorPagina;
+    var limSuperior = resultadosPorPagina.toString();
+    var limInferior = limiteInferior.toString();
+
     //Construir la lista de inmuebles
-    const inmuebles = await SearchPage.inmueblesPaginados(limiteInferior,resultadosPorPagina);
+    const inmuebles = await SearchPage.inmueblesPaginados(limInferior,limSuperior);
     for (let i=0; i < inmuebles[0].length; i++) {
-        const imgId = await SearchPage.idFotoPortada([0][i].idInmueble);
-        const imgSrc = await SearchPage.srcFotoPortada(imgId[0][0].archivoFoto);
+        const imgId = await SearchPage.idFotoPortada((inmuebles[0][i].idInmueble.toString()));
+        const imgSrc = await SearchPage.srcFotoPortada((imgId[0][0].idFoto).toString());
         inmuebles[0][i].img = imgSrc[0][0].archivoFoto
     }
     res.status(200).json({code: 200, msg:"Ok", data:inmuebles[0]})
