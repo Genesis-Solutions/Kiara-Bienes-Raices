e21c783fa8158fc19ba99525148e637f62e6574f
Jaime_prog <A00571842@tec.mx>
2023-04-21 12:21:04 -0600

    KIA-M-V-C-CORRECCION-INSERT-USER

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,93 +6,111 @@ const User = require("../models/user.model");
 
 // - Getter de la vista Login
 exports.getLogin = (req, res, next) => {
-    res.render("login", {
-        emailUsuario: req.session.emailUsuario ? req.session.emailUsuario : "",
-        info: "",
-    });
+  res.render("login", {
+    emailUsuario: req.session.emailUsuario ? req.session.emailUsuario : "",
+    info: "",
+  });
 };
 
 exports.login = (req, res, next) => {
-    User.findOne(req.body.emailUsuario)
-        .then(async ([rows, data]) => {
-            //Si no existe el correo, redirige a la pantalla de login
+  User.findOne(req.body.emailUsuario)
+    .then(async ([rows, data]) => {
+      //Si no existe el correo, redirige a la pantalla de login
 
-            if (rows.length < 1) {
-                return response.redirect("/login");
-            }
+      if (rows.length < 1) {
+        return response.redirect("/login");
+      }
 
-            // Información del usuario:
+      // Información del usuario:
 
-            req.session.isLoggedIn = true;
-            req.session.idUsuario = rows[0].idUsuario;
-            req.session.nombreUsuario = rows[0].nombre;
-            req.session.apellidosUsuario = rows[0].apellidosUsuario;
-            req.session.emailUsuario = rows[0].emailUsuario;
+      req.session.isLoggedIn = true;
+      req.session.idUsuario = rows[0].idUsuario;
+      req.session.nombreUsuario = rows[0].nombreUsuario;
+      req.session.apellidosUsuario = rows[0].apellidosUsuario;
+      req.session.emailUsuario = rows[0].emailUsuario;
+      req.session.idUsuario = rows[0].idUsuario;
+      req.session.idRol = rows[0].idRol;
 
-            // Contraseña del usuario:
+      // console.log("en método login " + req.session.nombreUsuario)
 
-            req.session.passwordUsuario = rows[0].passwordUsuario;
+      // Contraseña del usuario:
 
-            // Método de comparación para determinar autenticidad de la contraseña:
+      req.session.passwordUsuario = rows[0].passwordUsuario;
 
-            bcrypt
-                .compare(req.body.passwordUsuario, req.session.passwordUsuario)
-                .then((doMatch) => {
-                    if (doMatch) {
-                        console.log("success login");
-                        return res.redirect("./");
-                    } else {
-                        return res.redirect("/login");
-                    }
-                });
-        })
-        .catch((error) => {
-            console.log(error);
+      // Método de comparación para determinar autenticidad de la contraseña:
+
+      bcrypt
+        .compare(req.body.passwordUsuario, req.session.passwordUsuario)
+        .then((doMatch) => {
+          if (doMatch) {
+            // console.log('success login');
+            return res.redirect("./");
+          } else {
+            return res.redirect("/login");
+          }
         });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 // -- REGISTER -- //
 
+// - Getter de la vista Register
 exports.getRegister = (req, res, next) => {
-    res.render("register");
+  res.render("register");
 };
 
+// - Método de registro
 exports.register = (req, res, next) => {
-    const { nombreUsuario, apellidosUsuario, emailUsuario, telefonoUsuario, passwordUsuario, confirmPasswordUsuario, estadoCivilUsuario, ocupacionUsuario } = req.body;
-    const activoUsuario = 1;
-    const idRol = 2;
-    const idFoto = 1;
+  const {
+    nombreUsuario,
+    apellidosUsuario,
+    emailUsuario,
+    telefonoUsuario,
+    passwordUsuario,
+    confirmPasswordUsuario,
+    estadoCivilUsuario,
+    ocupacionUsuario,
+  } = req.body;
+  const activoUsuario = 1;
+  const idRol = 1;
+  const idFoto = 1;
+  telefonoUsuarioString = telefonoUsuario.toString();
+  activoUsuarioString = activoUsuario.toString();
+  idRolString = idRol.toString();
+  idFotoString = idFoto.toString();
 
-        console.log(req.body.emailUsuario)
-        User.findOne(req.body.emailUsuario)
-        .then(async ([rows, data]) => {
-            if (rows.length >= 1) {
-                console.log("El correo electrónico ya está registrado.");
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-
-    req.session.nombreUsuario = nombreUsuario;
-    req.session.apellidosUsuario = apellidosUsuario;
-    req.session.emailUsuario = emailUsuario;
-    req.session.telefonoUsuario = telefonoUsuario;
-    req.session.passwordUsuario = passwordUsuario;
-    req.session.confirmPasswordUsuario = confirmPasswordUsuario;
-    req.session.estadoCivilUsuario = estadoCivilUsuario;
-    req.session.ocupacionUsuario = ocupacionUsuario;
-
-
-
-    // Si todo fue validado correctamente, se inserta el usuario en la base de datos
-    console.log(apellidosUsuario);
-    User.insertUser(nombreUsuario, apellidosUsuario, emailUsuario, telefonoUsuario, passwordUsuario, estadoCivilUsuario, ocupacionUsuario, activoUsuario, idRol, idFoto
-    )
-        .then(() => {
+  // Revisar que el correo no esté registrado
+  User.findOne(emailUsuario)
+    .then(async ([rows, data]) => {
+      if (rows.length >= 1) {
+        console.log("El correo electrónico ya está registrado.");
+      } else {
+        console.log("El correo electrónico no está registrado.");
+        // Si todo fue validado correctamente, se inserta el usuario en la base de datos
+        User.insertUser(
+          nombreUsuario,
+          apellidosUsuario,
+          passwordUsuario,
+          telefonoUsuarioString,
+          emailUsuario,
+          estadoCivilUsuario,
+          ocupacionUsuario,
+          activoUsuarioString,
+          idRolString,
+          idFotoString
+        )
+          .then(() => {
             res.redirect("/");
-        })
-        .catch((error) => {
+          })
+          .catch((error) => {
             console.log(error);
-        });
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
--- models/user.model.js
+++ models/user.model.js
@@ -2,30 +2,6 @@ const db = require("../util/database");
 const bcrypt = require("bcryptjs");
 
 module.exports = class User {
-  constructor(
-    nombreUsuario,
-    apellidosUsuario,
-    passwordUsuario,
-    telefonoUsuario,
-    emailUsuario,
-    estadoCivilUsuario,
-    activoUsuario,
-    idRol,
-    idFoto,
-    ocupacionUsuario
-  ) {
-    this.nombreUsuario = nombreUsuario;
-    this.apellidosUsuario = apellidosUsuario;
-    this.passwordUsuario = passwordUsuario;
-    this.telefonoUsuario = telefonoUsuario;
-    this.emailUsuario = emailUsuario;
-    this.estadoCivilUsuario = estadoCivilUsuario;
-    this.activoUsuario = activoUsuario;
-    this.idRol = idRol;
-    this.idFoto = idFoto;
-    this.ocupacionUsuario = ocupacionUsuario;
-  }
-
   // Para verificar que existe el usuario
 
   static findOne(emailUsuario) {
@@ -34,24 +10,24 @@ module.exports = class User {
     ]);
   }
 
-  // Para registrar un nuevo usuario
-  static insertUser() {
-    return bcrypt.hash(this.passwordUsuario, 12).then((passwordCifrado) => {
-      return db.execute(
-        "INSERT INTO usuario(nombreUsuario, apellidosUsuario, emailUsuario,telefonoUsuario, passwordUsuario, estaodoCivilUsuario, ocupacionUsuario,activoUsuario ,idRol, idFoto) VALUES (?, ?, ?, ?, ?,?,?,?,?,?)",
-        [
-          this.nombreUsuario,
-          this.apellidosUsuario,
-          this.emailUsuario,
-          passwordCifrado,
-          this.telefonoUsuario,
-          this.estadoCivilUsuario,
-          this.ocupacionUsuario,
-          this.activoUsuario,
-          this.idRol,
-          this.idFoto,
-        ]
-      );
-    });
-  }
+    static insertUser(nombreUsuario, apellidosUsuario,passwordUsuario,telefonoUsuarioString, emailUsuario,estadoCivilUsuario, ocupacionUsuario,activoUsuarioString ,idRolString, idFotoString) {
+    return bcrypt.hash(passwordUsuario, 12)
+        .then((passwordCifrado)=> {
+            return db.execute('INSERT INTO usuario(nombreUsuario, apellidosUsuario,passwordUsuario,telefonoUsuario, emailUsuario,estadoCivilUsuario, ocupacionUsuario,activoUsuario ,idRol, idFoto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', 
+            [
+              nombreUsuario,
+              apellidosUsuario,
+              passwordCifrado,
+              telefonoUsuarioString,
+              emailUsuario,
+              estadoCivilUsuario,
+              ocupacionUsuario,
+              activoUsuarioString,
+              idRolString,
+              idFotoString,
+              ]).catch((error)=>{
+                console.log(error);
+            });
+        })
+}
 };
