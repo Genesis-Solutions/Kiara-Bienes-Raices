e2d647edcb5fa74e405309bcba036080404eae30
MawLr <mano_med_rod@hotmail.com>
2023-04-22 23:29:17 -0600

    KIA-A-Modifica config. de Tailwind

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(session({
 
 // Le decimos a node que nuestros assets se encuentran en assets
 app.use(express.static(path.join(__dirname, 'assets')))
-//app.use(express.static(path.join(__dirname, 'styles')))
+app.use(express.static(path.join(__dirname, 'styles')))
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
