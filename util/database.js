6851e27d143b3b53acceef05b55bc1e0e79b9786
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-04-26 01:47:24 -0600

    KIA-V-Arregla distribucion del front

--- util/database.js
+++ util/database.js
@@ -4,9 +4,9 @@ const { createPool } = require('mysql2');
 const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: 'ValarMelkor153*',
+    password: 'Pops11252001!',
     port: 3306,
-    database: 'kiara',
+    database: 'dev_kiara2',
 });
 
 module.exports = pool.promise();
