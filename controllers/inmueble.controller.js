5cc6f346d183cef49b715d6e97e5fa203333ccec
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-04-28 11:40:53 -0600

    KIA-V-R-Crea ruta a la vista de editar Inmueble

--- controllers/inmueble.controller.js
+++ controllers/inmueble.controller.js
@@ -46,3 +46,28 @@ exports.getImgFromBucket = ( req,res,next ) => {
         res.send(data.Body);
     });
 }
+
+exports.getEditarInmueble = async(req, res, next) => {
+    //Info de agente e inmueblee
+    const inmueble = await Inmueble.getInmueble(req.params.idInmueble);
+    const idAgente = await Inmueble.getIdAgente(req.params.idInmueble);
+    const agente = Inmueble.getInfoAgente(idAgente);
+    //Imagenes
+    const idFotos = await Inmueble.getIdFotosInmueble(req.params.idInmueble);
+    //console.log(idFotos[0]);
+    arregloFotos = [];
+    for (let i=0; i < idFotos[0].length; i++) {
+        //console.log(idFotos[0][i].idFoto);
+        const imgSrc = await Inmueble.getSrcFotosInmueble((idFotos[0][i].idFoto));
+        const imgSrcFilename = (imgSrc[0][0].archivoFoto).slice(23);
+        arregloFotos.push(imgSrcFilename);
+    }
+    //console.log(arregloFotos);
+    res.render('inmueble', {
+        fotos: arregloFotos,
+        inmuebles : inmueble,
+        agente : agente,
+        isLogged: req.session.isLoggedIn,
+        idRol: req.session.idRol
+    })
+}
--- routes/inmueble.routes.js
+++ routes/inmueble.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const isLogged = require('../util/isLogged.js')
+const adminAuth = require('../util/adminAuth.js')
 
 const inmuebleController = require('../controllers/inmueble.controller.js');
 
 //Obtener las imagenes del bucket de S3.
 router.get('/get_bucket_img',inmuebleController.getImgFromBucket);
 router.get('/:idInmueble', inmuebleController.getInmueble);
+
+//Modificar Inmueble
+router.get('/editarInmueble/:idInmueble', isLogged, adminAuth, inmuebleController.getEditarInmueble);
 
 module.exports = router;
