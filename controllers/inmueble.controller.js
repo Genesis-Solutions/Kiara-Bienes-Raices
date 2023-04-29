7d6063452efeb575e104662801edee642cd86e7f
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-04-28 20:59:29 -0600

    KIA-M-V-C-Conecta todo

--- controllers/inmueble.controller.js
+++ controllers/inmueble.controller.js
@@ -71,3 +71,481 @@ exports.getEditarInmueble = async(req, res, next) => {
         idRol: req.session.idRol
     })
 }
+
+// -- MODIFY A PROPERTY AGENT OR ADMIN --//
+exports.updateBodyCasa = (req,res,next) => {
+    console.log("Entrando a la ruta update body casa");
+    //Elementos obligatorios del formulario
+    const {
+        titulo,
+        linkVideo,
+        m2terreno,
+        niveles,
+        mediosBanios,
+        cuotaMantenimiento,
+        fechaConstruccion,
+        usoSuelo,
+        ubicado,
+        tipoGas,
+        m2construccion,
+        recamaras,
+        estacionamientos,
+        banios,
+        desc,
+        direccion,
+        linkMaps,
+    } = req.body;
+    //Obtener el tipo de movimiento y los respectivos precios
+    const venta = req.body.venta ? 1 : 0;
+    const renta = req.body.renta ? 1 : 0;
+    let tipoMovimiento = 0;
+    let precioVenta = 0;
+    let precioRenta = 0;
+    if(venta === 1 && renta === 1) {
+        tipoMovimiento = 3
+        precioVenta = req.body.precioVenta;
+        precioRenta = req.body.precioRenta;
+    } else if (venta === 1 && renta === 0) {
+        tipoMovimiento = 1
+        precioVenta = req.body.precioVenta;
+    }else if (venta === 0 && renta === 1) {
+        tipoMovimiento = 2
+        precioRenta = req.body.precioRenta;
+    }
+    //Obtener amenidades adicionales
+    const cocina = req.body.cocina ? 1 : 0;
+    const cisterna = req.body.cisterna ? 1 : 0;
+    const cuartoServicio = req.body.cuartoServicio ? 1 : 0;
+    const salaTV = req.body.salaTV ? 1 : 0;
+    const estudio = req.body.estudio ? 1 : 0;
+    const roofGarden = req.body.roofGarden ? 1 : 0;
+    const areaLavado = req.body.areaLavado ? 1 : 0;
+    const vigilancia = req.body.vigilancia ? 1 : 0;
+    const jardin = req.body.jardin ? 1 : 0;
+    const bodega = req.body.bodega ? 1 : 0;
+    console.log(req.body);
+    console.log("tipoMovimiento",tipoMovimiento);
+    console.log("precioVenta",precioVenta);
+    console.log("precioRenta",precioRenta);
+    console.log("cocina",cocina);
+    console.log("cisterna",cisterna);
+    console.log("cuartoServicio",cuartoServicio);
+    console.log("salaTV",salaTV);
+    console.log("estudio",estudio);
+    console.log("roofGarden",roofGarden);
+    console.log("areaLavado",areaLavado);
+    console.log("vigilancia",vigilancia);
+    console.log("jardin",jardin);
+    console.log("bodega",bodega);
+    const idInmueble = req.params.idInmueble;
+    console.log("idInmueble",idInmueble);
+    Inmueble.changeInmuebleCasa(
+        titulo,
+        tipoMovimiento,
+        linkVideo,
+        precioVenta,
+        precioRenta,
+        m2terreno,
+        m2construccion,
+        niveles,
+        mediosBanios,
+        cuotaMantenimiento,
+        fechaConstruccion,
+        usoSuelo,
+        ubicado,
+        tipoGas,
+        recamaras,
+        estacionamientos,
+        banios,
+        desc,
+        cocina,
+        cisterna,
+        cuartoServicio,
+        salaTV,
+        estudio,
+        roofGarden,
+        areaLavado,
+        vigilancia,
+        jardin,
+        bodega,
+        direccion,
+        linkMaps,
+        idInmueble
+    );
+    res.redirect('/inmueble/'+idInmueble);
+};
+
+exports.updateBodyLocal = (req,res,next) => {
+    console.log("Entrando a la ruta update body local");
+    const {
+        titulo,
+        linkVideo,
+        m2terreno,
+        m2construccion,
+        medidaFrente,
+        medidaFondo,
+        niveles,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        fechaConstruccion,
+        tipoGas,
+        estacionamientos,
+        banios,
+        direccion,
+        linkMaps,
+        desc
+    } = req.body;
+    //Obtener el tipo de movimiento y los respectivos precios
+    //Obtener el tipo de movimiento y los respectivos precios
+    const venta = req.body.venta ? 1 : 0;
+    const renta = req.body.renta ? 1 : 0;
+    let tipoMovimiento = 0;
+    let precioVenta = 0;
+    let precioRenta = 0;
+    if(venta === 1 && renta === 1) {
+        tipoMovimiento = 3
+        precioVenta = req.body.precioVenta;
+        precioRenta = req.body.precioRenta;
+    } else if (venta === 1 && renta === 0) {
+        tipoMovimiento = 1
+        precioVenta = req.body.precioVenta;
+    }else if (venta === 0 && renta === 1) {
+        tipoMovimiento = 2
+        precioRenta = req.body.precioRenta;
+    }
+    const cocina = req.body.cocina ? 1 : 0;
+    const cisterna = req.body.cisterna ? 1 : 0;
+    const cuartoServicio = req.body.cuartoServicio ? 1 : 0;
+    const vigilancia = req.body.vigilancia ? 1 : 0;
+    const idInmueble = req.params.idInmueble;
+    console.log("idInmueble",idInmueble);
+    Inmueble.changeInmuebleLocal(
+        titulo,
+        tipoMovimiento,
+        linkVideo,
+        precioVenta,
+        precioRenta,
+        m2terreno,
+        m2construccion,
+        medidaFrente,
+        medidaFondo,
+        niveles,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        cocina,
+        cisterna,
+        cuartoServicio,
+        fechaConstruccion,
+        vigilancia,
+        tipoGas,
+        estacionamientos,
+        banios,
+        desc,
+        direccion,
+        linkMaps,
+        idInmueble
+    );
+    res.redirect('/inmueble/'+idInmueble);
+};
+
+exports.updateBodyTerreno = (req,res,next) => {
+    console.log("Entrando a la ruta update body terreno");
+    const {
+        titulo,
+        linkVideo,
+        m2terreno,
+        m2construccion,
+        medidaFrente,
+        medidaFondo,
+        usoSuelo,
+        ubicado,
+        tipoSuelo,
+        cuotaMantenimiento,
+        direccion,
+        linkMaps,
+        desc,
+    } = req.body;
+    //Obtener el tipo de movimiento y los respectivos precios
+    const venta = req.body.venta ? 1 : 0;
+    const renta = req.body.renta ? 1 : 0;
+    let tipoMovimiento = 0;
+    let precioVenta = 0;
+    let precioRenta = 0;
+    if(venta === 1 && renta === 1) {
+        tipoMovimiento = 3
+        precioVenta = req.body.precioVenta;
+        precioRenta = req.body.precioRenta;
+    } else if (venta === 1 && renta === 0) {
+        tipoMovimiento = 1
+        precioVenta = req.body.precioVenta;
+    }else if (venta === 0 && renta === 1) {
+        tipoMovimiento = 2
+        precioRenta = req.body.precioRenta;
+    }
+    const servicioAgua = req.body.servicioAgua ? 1 : 0;
+    const servicioLuz = req.body.servicioLuz ? 1 : 0;
+    const servicioDrenaje = req.body.servicioDrenaje ? 1 : 0;
+    const vigilancia = req.body.vigilancia ? 1 : 0;
+    const idInmueble = req.params.idInmueble;
+    console.log("idInmueble",idInmueble);
+    Inmueble.changeInmuebleTerreno(
+        titulo,
+        tipoMovimiento,
+        linkVideo,
+        precioVenta,
+        precioRenta,
+        m2terreno,
+        m2construccion,
+        medidaFrente,
+        medidaFondo,
+        usoSuelo,
+        ubicado,
+        servicioAgua,
+        servicioLuz,
+        servicioDrenaje,
+        tipoSuelo,
+        cuotaMantenimiento,
+        vigilancia,
+        desc,
+        direccion,
+        linkMaps,
+        idInmueble
+    );
+    res.redirect('/inmueble/'+idInmueble);
+};
+
+exports.updateBodyBodega = (req,res,next) => {
+    console.log("Entrando a la ruta update body bodega");
+    const {
+        titulo,
+        linkVideo,
+        m2terreno,
+        m2construccion,
+        medidaFrente,
+        medidaFondo,
+        niveles,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        fechaConstruccion,
+        muros,
+        altura,
+        tipoPiso,
+        tipoLuz,
+        estacionamientos,
+        banios,
+        direccion,
+        linkMaps,
+        desc
+    } = req.body;
+    //Obtener el tipo de movimiento y los respectivos precios
+    const venta = req.body.venta ? 1 : 0;
+    const renta = req.body.renta ? 1 : 0;
+    let tipoMovimiento = 0;
+    let precioVenta = 0;
+    let precioRenta = 0;
+    if(venta === 1 && renta === 1) {
+        tipoMovimiento = 3
+        precioVenta = req.body.precioVenta;
+        precioRenta = req.body.precioRenta;
+    } else if (venta === 1 && renta === 0) {
+        tipoMovimiento = 1
+        precioVenta = req.body.precioVenta;
+    }else if (venta === 0 && renta === 1) {
+        tipoMovimiento = 2
+        precioRenta = req.body.precioRenta;
+    }
+    const cocina = req.body.cocina ? 1 : 0;
+    const cisterna = req.body.cisterna ? 1 : 0;
+    const vigilancia = req.body.vigilancia ? 1 : 0;
+    const generadorElectrico = req.body.generadorElectrico ? 1 : 0;
+    const andenCarga = req.body.andenCarga ? 1 : 0;
+    const oficina = req.body.oficina ? 1 : 0;
+    const patioManiobras = req.body.patioManiobras ? 1 : 0;
+    const idInmueble = req.params.idInmueble;
+    console.log("idInmueble",idInmueble);
+    Inmueble.changeInmuebleBodega(
+        titulo,
+        tipoMovimiento,
+        linkVideo,
+        precioVenta,
+        precioRenta,
+        m2terreno,
+        m2construccion,
+        medidaFrente,
+        medidaFondo,
+        niveles,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        estacionamientos,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        cocina,
+        cisterna,
+        fechaConstruccion,
+        vigilancia,
+        generadorElectrico,
+        andenCarga,
+        oficina,
+        patioManiobras,
+        muros,
+        altura,
+        tipoPiso,
+        tipoLuz,
+        banios,
+        desc,
+        direccion,
+        linkMaps,
+        idInmueble
+    );
+    res.redirect('/inmueble/'+idInmueble);
+};
+
+exports.updateBodyOficina = (req,res,next) => {
+    console.log("Entrando a la ruta update body oficina");
+    const {
+        titulo,
+        linkVideo,
+        m2terreno,
+        m2construccion,
+        niveles,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        fechaConstruccion,
+        estacionamientos,
+        banios,
+        direccion,
+        linkMaps,
+        desc
+    } = req.body;
+    //Obtener el tipo de movimiento y los respectivos precios
+    const venta = req.body.venta ? 1 : 0;
+    const renta = req.body.renta ? 1 : 0;
+    let tipoMovimiento = 0;
+    let precioVenta = 0;
+    let precioRenta = 0;
+    if(venta === 1 && renta === 1) {
+        tipoMovimiento = 3
+        precioVenta = req.body.precioVenta;
+        precioRenta = req.body.precioRenta;
+    } else if (venta === 1 && renta === 0) {
+        tipoMovimiento = 1
+        precioVenta = req.body.precioVenta;
+    }else if (venta === 0 && renta === 1) {
+        tipoMovimiento = 2
+        precioRenta = req.body.precioRenta;
+    }
+    const cocina = req.body.cocina ? 1 : 0;
+    const cisterna = req.body.cisterna ? 1 : 0;
+    const vigilancia = req.body.vigilancia ? 1 : 0;
+    const idInmueble = req.params.idInmueble;
+    console.log("idInmueble",idInmueble);
+    Inmueble.changeInmuebleOficina(
+        titulo,
+        tipoMovimiento,
+        linkVideo,
+        precioVenta,
+        precioRenta,
+        m2terreno,
+        m2construccion,
+        niveles,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        cocina,
+        cisterna,
+        fechaConstruccion,
+        vigilancia,
+        estacionamientos,
+        banios,
+        direccion,
+        desc,
+        linkMaps,
+        idInmueble
+    );
+    res.redirect('/inmueble/'+idInmueble);
+};
+
+exports.updateBodyOtra = (req,res,next) => {
+    console.log("Entrando a la ruta update body otra");
+    const {
+        titulo,
+        linkVideo,
+        m2terreno,
+        m2construccion,
+        niveles,
+        recamaras,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        estacionamientos,
+        banios,
+        direccion,
+        linkMaps,
+        desc
+    } = req.body;
+    //Obtener el tipo de movimiento y los respectivos precios
+    const venta = req.body.venta ? 1 : 0;
+    const renta = req.body.renta ? 1 : 0;
+    let tipoMovimiento = 0;
+    let precioVenta = 0;
+    let precioRenta = 0;
+    if(venta === 1 && renta === 1) {
+        tipoMovimiento = 3
+        precioVenta = req.body.precioVenta;
+        precioRenta = req.body.precioRenta;
+    } else if (venta === 1 && renta === 0) {
+        tipoMovimiento = 1
+        precioVenta = req.body.precioVenta;
+    }else if (venta === 0 && renta === 1) {
+        tipoMovimiento = 2
+        precioRenta = req.body.precioRenta;
+    }
+    const cocina = req.body.cocina ? 1 : 0;
+    const cisterna = req.body.cisterna ? 1 : 0;
+    const vigilancia = req.body.vigilancia ? 1 : 0;
+    const idInmueble = req.params.idInmueble;
+    console.log("idInmueble",idInmueble);
+    Inmueble.changeInmuebleOtra(
+        titulo,
+        tipoMovimiento,
+        linkVideo,
+        precioVenta,
+        precioRenta,
+        m2terreno,
+        m2construccion,
+        niveles,
+        recamaras,
+        cuartosPrivadosInmueble,
+        mediosBanios,
+        usoSuelo,
+        ubicado,
+        cuotaMantenimiento,
+        cocina,
+        cisterna,
+        vigilancia,
+        estacionamientos,
+        banios,
+        desc,
+        direccion,
+        linkMaps,
+        idInmueble
+    );
+    res.redirect('/inmueble/'+idInmueble);
+};
--- routes/inmueble.routes.js
+++ routes/inmueble.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isLogged = require('../util/isLogged.js')
 const adminAuth = require('../util/adminAuth.js')
+const agenteAdminAuth = require('../util/agenteAdminAuth.js')
 
 const inmuebleController = require('../controllers/inmueble.controller.js');
 
@@ -10,6 +11,12 @@ router.get('/get_bucket_img',inmuebleController.getImgFromBucket);
 router.get('/:idInmueble', inmuebleController.getInmueble);
 
 //Modificar Inmueble
-router.get('/editarInmueble/:idInmueble', isLogged, adminAuth, inmuebleController.getEditarInmueble);
+router.get('/editarInmueble/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.getEditarInmueble);
+router.post('/editarInmueble/casaDepartamento/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.updateBodyCasa);
+router.post('/editarInmueble/local/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.updateBodyLocal);
+router.post('/editarInmueble/terreno/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.updateBodyTerreno);
+router.post('/editarInmueble/bodega/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.updateBodyBodega);
+router.post('/editarInmueble/oficina/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.updateBodyOficina);
+router.post('/editarInmueble/otro/:idInmueble', isLogged, agenteAdminAuth, inmuebleController.updateBodyOtra);
 
 module.exports = router;
