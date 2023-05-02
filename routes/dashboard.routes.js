15cf861210b0724d277da24dbdc72a42d87ec9f5
Jose <A01610805@tec.mx>
2023-05-01 19:39:28 -0600

    KIA-MVC-finishFormsPartials

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -11,12 +11,18 @@ router.post('/alta/inmueble/imagenPrincipal/:inmueble', dashboardController.setM
 //Actulizar el cuerpo de una casa
 router.post('/alta/inmueble/cuerpoCasa/:inmueble', dashboardController.updateBodyCasa);
 
-//Actulizar el cuerpo de un departamento
+//Actualizar el cuerpo de un departamento
 router.post('/alta/inmueble/cuerpoDepartamento/:inmueble', dashboardController.updateBodyCasa);
-//Actulizar el cuerpo de una bodega
+//Actualizar el cuerpo de una bodega
 router.post('/alta/inmueble/cuerpoBodega/:inmueble', dashboardController.updateBodyBodega);
-//Actulizar el cuerpo de un terreno
+//Actualizar el cuerpo de un terreno
 router.post('/alta/inmueble/cuerpoTerreno/:inmueble', dashboardController.updateBodyTerreno);
+//Actualizar el cuerpo de un local
+router.post('/alta/inmueble/cuerpoLocal/:inmueble', dashboardController.updateBodyLocal);
+//Actualizar el cuerpo de un oficina
+router.post('/alta/inmueble/cuerpoOficina/:inmueble', dashboardController.updateBodyOficina);
+//Actualizar el cuerpo de un otro
+router.post('/alta/inmueble/cuerpoOtro/:inmueble', dashboardController.updateBodyOtro);
 //Poner el resto de inmuebles....
 
 //Ver el formulario dependiendo de la categoria
