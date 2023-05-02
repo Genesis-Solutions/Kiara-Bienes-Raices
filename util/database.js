9fb44afa5edef608b0f75a5fe69b22fc180f0005
Halim <a01769137@tec.mx>
2023-05-01 23:25:13 -0600

    KIA-V-Fix vista lista usuario

--- util/database.js
+++ util/database.js
@@ -1,1 +1,13 @@
 //Aqui va la conexion a la base de datos
+
+const { createPool } = require("mysql2");
+
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: '',
+});
+
+module.exports = pool.promise();
