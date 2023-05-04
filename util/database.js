001989833194573453ba0fd7f760341fb23cfa29
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-05-04 17:07:04 -0600

    Quita mi base de datos

--- util/database.js
+++ util/database.js
@@ -1,12 +1,1 @@
 //Aqui va la conexion a la base de datos\const { createPool } = require('mysql2');
-const { createPool } = require('mysql2');
-
-const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Pops11252001!',
-    port: 3306,
-    database: 'dev_kiara1.2',
-});
-
-module.exports = pool.promise();
