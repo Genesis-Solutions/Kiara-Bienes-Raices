be44bc668adf7150cf594360ebf9a0783c190969
Said Ortigoza <a01707430@tec.mx>
2023-05-08 15:54:23 -0600

    KIA-V-Added responsiveness to searchpage

--- util/database.util.js
+++ util/database.util.js
@@ -1,1 +1,13 @@
 //Aqui va la conexion a la base de datos
+
+const { createPool } = require("mysql2");
+
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'env-77-QSD23',
+    port: 3306,
+    database: 'dev_kiara',
+});
+
+module.exports = pool.promise();
