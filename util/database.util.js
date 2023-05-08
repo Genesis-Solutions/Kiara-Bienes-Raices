5d31ca63cafcf4371119d34122cd6be1d2d4f4e7
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-05-08 16:50:49 -0600

    KIA-V-Fix Pisos repetidos y placeholder de precios con value

--- util/database.util.js
+++ util/database.util.js
@@ -1,1 +1,12 @@
 //Aqui va la conexion a la base de datos
+const { createPool } = require('mysql2');
+
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Pops11252001!',
+    port: '3306',
+    database: 'dev_kiara1.3',
+});
+
+module.exports = pool.promise();
