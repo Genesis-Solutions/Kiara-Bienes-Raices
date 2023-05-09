2d0f18e15a96808252dc90b6086c4df659525e6f
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-05-09 12:46:48 -0600

    KIA-V-Fixes TipoMovimiento en 0

--- util/database.util.js
+++ util/database.util.js
@@ -1,1 +1,12 @@
 //Aqui va la conexion a la base de datos
+const { createPool } = require('mysql2');
+
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Pops11252001!',
+    port: 3306,
+    database: 'dev_kiara1.4'
+})
+
+module.exports = pool.promise();
