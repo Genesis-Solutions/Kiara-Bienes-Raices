38587b828777fd6849bc7dc96c5b1798e9c93c94
Jose <A01610805@tec.mx>
2023-05-10 11:29:34 -0600

    fix query para obtención de los últimos 4 inmuebles

--- util/database.util.js
+++ util/database.util.js
@@ -1,1 +1,13 @@
 //Aqui va la conexion a la base de datos
+
+const { createPool } = require("mysql2");
+
+const pool = createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'dev_kiara_2',
+});
+
+module.exports = pool.promise();
