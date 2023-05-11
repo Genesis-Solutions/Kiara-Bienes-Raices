60e1f938a8e8625a6f7eb3b2c8c4a7158b78151d
MawLr <mano_med_rod@hotmail.com>
2023-05-10 23:36:13 -0600

    KIA-ENV-Agrega DB_HOST

--- util/database.util.js
+++ util/database.util.js
@@ -3,7 +3,7 @@
 const { createPool } = require("mysql2");
 
 const pool = createPool({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
