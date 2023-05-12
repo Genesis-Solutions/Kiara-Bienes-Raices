6b5fe1d594b99671b8d253b05ac54aa2fb2b727c
ATG-5222 <A01275222@tec.mx>
2023-05-12 14:30:33 -0600

    KIA-C-renombrar variable

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -27,7 +27,7 @@ exports.postLocalImage = (req,res,next) => {
 }
 
 exports.postS3SingleImage = (req,res,next) => {
-    var upload = storage.array('uploadedImages[]', 1);
+    var upload = storage.array('uploadedImages', 1);
     upload(req, res, function (err) {
         if (err) {
             console.log(err);
