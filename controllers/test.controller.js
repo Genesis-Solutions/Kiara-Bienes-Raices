b549aeb13b27385fb6594409d760ad8f03b3de15
MawLr <mano_med_rod@hotmail.com>
2023-05-12 23:34:37 -0600

    KIA-FIX-Agrega directamente FIX-S3-UPLOAD a dev

---
+++ controllers/test.controller.js
@@ -0,0 +1,100 @@
+const Test = require('../models/test.model');
+const { uploadMedia } = require('../util/testLocalMulter.util');
+const { storage } = require('../util/awsMediaMulter.util');
+
+exports.getTestPage = (req,res,next) => {
+    // Renderizar la vista de test
+    res.render("testImageUpload", {
+        isLogged: req.session.isLoggedIn,
+        idRol: req.session.idRol,
+    });
+};
+
+exports.postLocalImage = (req,res,next) => {
+    var upload = uploadMedia.array('mediaLocal',1);
+    upload(req,res,function(err) {
+        if(err) {
+            console.log(err);
+            return res.end("Error uploading file.");
+        }
+        //var pathDest = req.files[0].destination.slice(8);
+        const filename = req.files[0].filename;
+        //console.log(pathDest);
+        console.log(filename);
+        //res.status(200).json({code: 200, msg:"Ok"}); 
+        res.redirect('/test');
+    })
+}
+
+exports.postS3SingleImage = (req,res,next) => {
+    var upload = storage.array('singleMediaS3', 1);
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+        }
+        const mediaName = file.key;
+        console.log("-------");
+        console.log(mediaName);
+        console.log("Desde el upload de single image");
+    });
+    //res.status(200).json({code: 200, msg:"Ok"}); 
+    res.redirect('/test');
+};
+
+exports.postS3MultipleImages = (req,res,next) => {
+    // console.log("Entrando a multiple images");
+    // console.log("Antes del log de req.body");
+    // console.log(req.body);
+    // console.log("Antes del log de req.files");
+    // console.log(req.files);
+    var upload = storage.array('uploadedImages[]');
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            req.files.forEach(function (file) {
+                const mediaName = file.key;
+                console.log("-------");
+                console.log("mediaName: ",mediaName);
+                console.log("Desde el multiple upload a s3");
+            });
+        }
+    });
+    ///res.status(200).json({code: 200, msg:"Ok"}); 
+    res.redirect('/test');
+};
+
+exports.postDropzoneLocal = (req,res,next) => {
+    var upload = uploadMedia.array('mediaDropzoneLocal', 25);
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            req.files.forEach(function (file) {
+                //var pathDest = req.files[0].destination.slice(8);
+                const filename = req.files[0].filename;
+                //console.log(pathDest);
+                console.log(filename);
+            });
+        }
+    });
+    //res.status(200).json({code: 200, msg:"Ok"}); 
+    res.redirect('/test');
+};
+
+exports.postDropzoneS3 = (req, res, next) => {
+    var upload = storage.array('mediaDropzoneS3', 25);
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            req.files.forEach(function (file) {
+                const mediaName = file.key;
+                console.log(mediaName);
+                console.log("Desde el upload de dropzone a s3");
+            });
+        }
+    });
+    //res.status(200).json({code: 200, msg:"Ok"}); 
+    res.redirect('/test');
+};
---
+++ routes/test.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const testController = require('../controllers/test.controller');
+
+/*
+* Rutas de prueba de subida de imagenes al S3 desde EC2.
+*/
+
+//Subir por medio de multiple upload al bucket de s3
+router.post('/multipleImages',testController.postS3MultipleImages);
+//Subir por medio un multer aws un dropzone a s3
+router.post('/dropzoneS3',testController.postDropzoneS3);
+//Subir por medio un multer simple un dropzone a assets/file
+router.post('/dropzonelocal',testController.postDropzoneLocal);
+//Subir por medio un multer aws un solo archivo al s3.
+router.post('/singleUploadS3',testController.postS3SingleImage);
+//Subir por medio un multer simple a assets/file
+router.post('/createlocal',testController.postLocalImage);
+router.get('/', testController.getTestPage);
+
+module.exports = router;
