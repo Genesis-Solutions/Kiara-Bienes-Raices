2a014166d5f4e1e83d6e323ccc2ee58992ced205
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-05-13 13:43:24 -0600

    KIA-2-8-Fixes Carrusel

--- util/database.util.js
+++ util/database.util.js
@@ -3,11 +3,11 @@
 const { createPool } = require('mysql2');
 
 const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    database: process.env.DB_PROD,
+    host: 'localhost',
+    user: 'root',
+    password: 'Pops11252001!',
+    port: 3306,
+    database: 'dev_kiara',
 });
 
 module.exports = pool.promise();
