c4e14cd8e3625b927739e2d402350c5c53f6719f
Halim <a01769137@tec.mx>
2023-05-29 13:05:52 -0600

    KIA-U-Fix ruta

--- util/email.js
+++ util/email.js
@@ -12,7 +12,7 @@ exports.olvidePassword = (datos) => {
                 <h1> Hola! ${nombre}, solicitaste cambiar tu contraseña <h1>
 
                 <p>Sigue el siguiente enlace para reestablercer tu contraseña:
-                <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/olvideContra/${token}">Reestablece la contraseña</a> </p>
+                <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/olvidePassword/${token}">Reestablece la contraseña</a> </p>
 
                 <p>Si tú no solicitaste el cambio, descarta este email</p>
     `
