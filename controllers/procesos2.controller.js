f7657ab3a3dd2c756a6aeb72278fcc73dcb9bdf0
CamalichM <99758150+CamalichM@users.noreply.github.com>
2023-05-30 12:09:09 -0600

    KIA.MVC-Queries de inicialización y plantillas listas

--- controllers/procesos2.controller.js
+++ controllers/procesos2.controller.js
@@ -2,10 +2,14 @@
 const Proceso = require('../models/procesos2.model');
 
 exports.getIniciarProceso = async (req, res, next) => {
+    const inmueble = await Proceso.fetchInmueble(req.params.idInmueble);
+    const usuarios = await Proceso.fetchAllClients();
     res.render('procesosIniciar', {
     isLogged: req.session.isLoggedIn,
     idRol: req.session.idRol,
+    idAgente: req.session.idUsuario,
     idTipoMovimiento: req.params.idTipoMovimiento,
-    idInmueble: req.params.idInmueble,
+    inmueble: inmueble,
+    usuarios: usuarios
 });
 };
