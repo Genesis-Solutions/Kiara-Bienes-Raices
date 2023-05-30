cdf36acbb469d8bab22bf15815515edd8295e136
Ricardo Cáceres <83652905+Caceres-A01706972@users.noreply.github.com>
2023-05-29 19:47:33 -0600

    KIA-C-Fix-warning

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -80,7 +80,10 @@ exports.login = (req, res, next) => {
 
 // - Getter de la vista Register
 exports.getRegister = (req, res, next) => {
-  res.render("register");
+  res.render("register", {
+    warning : req.flash('warning'),
+    success : req.flash('success')
+  });
 };
 
 // - Método de registro
