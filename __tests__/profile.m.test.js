9d7c2cef8d491dc5402da8885dc8d4240be081f9
ATG-5222 <A01275222@tec.mx>
2023-06-02 12:12:50 -0600

    KIA-T-añadir prueba de jest en profile.m.test.js

--- __tests__/profile.m.test.js
+++ __tests__/profile.m.test.js
@@ -32,3 +32,22 @@ describe('getUserProfile', () => {
         }]);
     });
 });
+
+describe('changeUserInfo', () => {
+    it('debe ejecutar la consulta SQL correcta para actualizar la información del usuario', async () => {
+        const nombre = 'Nombre del usuario';
+        const apellidos = 'Apellidos del usuario';
+        const email = 'Email del usuario';
+        const telefono = 'Telefono del usuario';
+        const estadoCivilUsuario = 'Estado civil del usuario';
+        const ocupacionUsuario = 'Ocupacion del usuario';
+        const idUsuario = 1;
+
+        await User.changeUserInfo(nombre, apellidos, email, telefono, estadoCivilUsuario, ocupacionUsuario, idUsuario);
+
+        expect(db.execute).toHaveBeenCalledWith(
+            'UPDATE usuario SET nombreUsuario=?, apellidosUsuario=?, emailUsuario=?, telefonoUsuario=?, estadoCivilUsuario=?, ocupacionUsuario=? WHERE idUsuario=?',
+            [nombre, apellidos, email, telefono, estadoCivilUsuario, ocupacionUsuario, idUsuario]
+        );
+    });
+});
