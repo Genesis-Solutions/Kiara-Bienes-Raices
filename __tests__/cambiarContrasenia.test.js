6f47aa160da6873e43d3cf95176c44c28a85254f
MawLr <mano_med_rod@hotmail.com>
2023-06-06 12:49:33 -0600

    KIA-TEST-prueba cambiar contraseña

---
+++ __tests__/cambiarContrasenia.test.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const db = require('../util/database.util.js'); 
+const {User, Token} = require('../models/user.model');
+
+// Mock para db.execute
+jest.mock('../util/database.util.js', () => ({
+    execute: jest.fn().mockResolvedValue([{ 
+        idUsuario: 1,
+        nombreUsuario: 'Marco',
+        apellidosUsuario: 'Antonio',
+        passwordUsuario: '1234',
+        telefonoUsuario: '4421548752',
+        emailUsuario: 'marco@test',
+        estadoCivilUsuario: 'N/A',
+        ocupacionUsuario: 'Programador',
+        activoUsuario: 1,
+        idRol: 1,
+        idFoto: 1
+    }])
+}));
+
+describe('Prueba de cambio de contraseña', () => {
+    it('debe cambiar la contraseña del usuario', async () => {
+        const newPassword = "chupo";
+        const emailUsuario = "marco@test"
+        const result = await User.resetPassword(newPassword, emailUsuario);
+
+        bcrypt.hash(newPassword, 12).then((passwordCifrado) => {
+            return expect(db.execute).toHaveBeenCalledWith('UPDATE usuario SET passwordUsuario = ? WHERE emailUsuario = ?', [passwordCifrado, emailUsuario])
+        })
+    })
+})
+
+
