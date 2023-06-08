e3f8a998dc23321b185661489131117a2cc24846
MawLr <mano_med_rod@hotmail.com>
2023-06-08 12:42:11 -0600

    KIA-R-Regresa rutas a la normalidad

--- routes/homepage.routes.js
+++ routes/homepage.routes.js
@@ -22,7 +22,6 @@ router.get('/nosotros', userController.getNosotros);
 router.get('/servicios', userController.getServicios);
 router.get('/search', homeController.getInmueblesFiltradosIndex);
 router.post('/search', homeController.getInmueblesFiltradosIndex);
-router.get('/search', homeController.getDepartamentos)
 router.get('/olvidePassword', userController.getOlvidePassword);
 router.post('/olvidePassword', userController.resetPassword);
 router.get('/olvidePassword/:token', userController.comprobarToken)
